0bd2fe1588b73769dae1f86ca8fa7ee2c2371c73
Sehyun Chung <sehyun.chung@icloud.com>
2024-01-03 21:56:30 +0900

    UPDATED: Added index.html.heex template
    - Created a new file called index.html.heex in the controllers/hello_html directory
    - Added a section with a heading "Hello World from Phoenix!"

--- lib/hello_phoenix_web/controllers/hello_html.ex
+++ lib/hello_phoenix_web/controllers/hello_html.ex
@@ -1,12 +1,5 @@
 defmodule HelloPhoenixWeb.HelloHTML do
   use HelloPhoenixWeb, :html
-  @moduledoc """
-  This is the HelloHTML controller.
-  It handles the rendering of the Hello! page.
-  """
-  def index(assigns) do
-    ~H"""
-    Hello!
-    """
-  end
+
+  embed_templates "hello_html/*"
 end
