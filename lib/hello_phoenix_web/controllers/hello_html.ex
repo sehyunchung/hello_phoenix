ea56fb46547651fdfbc9b2ba9a55a9df27419c1e
Sehyun Chung <sehyun.chung@icloud.com>
2024-01-03 10:59:44 +0900

    ADDED: Documentation for HelloHTML controller

--- lib/hello_phoenix_web/controllers/hello_html.ex
+++ lib/hello_phoenix_web/controllers/hello_html.ex
@@ -1,6 +1,9 @@
 defmodule HelloPhoenixWeb.HelloHTML do
   use HelloPhoenixWeb, :html
-
+  @moduledoc """
+  This is the HelloHTML controller.
+  It handles the rendering of the Hello! page.
+  """
   def index(assigns) do
     ~H"""
     Hello!
