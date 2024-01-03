ada8aa5a751276880b510bc8ee2bfd6ed80d8da7
Sehyun Chung <sehyun.chung@icloud.com>
2024-01-03 22:34:47 +0900

    ADDED: Introspect plug to endpoint.ex
    - Added introspect plug to endpoint.ex to print verb, host, and headers information

--- lib/hello_phoenix_web/endpoint.ex
+++ lib/hello_phoenix_web/endpoint.ex
@@ -47,5 +47,16 @@ defmodule HelloPhoenixWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug :introspect
   plug HelloPhoenixWeb.Router
+
+  def introspect(conn, _opts) do
+    IO.puts """
+    Verb: #{inspect(conn.method)}
+    Host: #{inspect(conn.host)}
+    Headers: #{inspect(conn.req_headers)}
+    """
+
+    conn
+  end
 end
