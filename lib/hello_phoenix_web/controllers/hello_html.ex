067160635094b5eec872a076afc3ba70863fb976
Sehyun Chung <sehyun.chung@icloud.com>
2024-01-07 17:37:19 +0900

    UPDATED: Removed redundant code new function in hello_html.ex
    - Removed the attr macro and added a new function called greet with a template
    - The greet function renders a HTML template that includes the value of the @messenger variable
    - Updated the show.html.heex file to use the greet function with the @messenger variable

--- lib/hello_phoenix_web/controllers/hello_html.ex
+++ lib/hello_phoenix_web/controllers/hello_html.ex
@@ -2,4 +2,12 @@ defmodule HelloPhoenixWeb.HelloHTML do
   use HelloPhoenixWeb, :html
 
   embed_templates "hello_html/*"
+
+  attr :messenger, :string, default: nil
+
+  def greet(assigns) do
+    ~H"""
+    <h2>Hello World, from <%= @messenger %>!</h2>
+    """
+  end
 end
